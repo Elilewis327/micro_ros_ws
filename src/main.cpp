30b817f4298f49ce644cba9ca211cc92f815b7a0
CalvinCRFC <CalvinCRFC@gmail.com>
2021-09-20 21:56:03 -0400

    Attempted header: frame works, not time

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,10 @@
 #include <rcl/error_handling.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
+#include <std_msgs/msg/header.h>
+// #include <drive_controller_msgs/msg/tank.h>
 
-#include <drive_controller_msgs/msg/tank.h>
-
-rcl_subscription_t subscriber;
-drive_controller_msgs__msg__Tank msg;
-rclc_executor_t executor;
-rclc_support_t support;
-rcl_allocator_t allocator;
-rcl_node_t node;
-rcl_timer_t timer;
+static std_msgs__msg__Header msg;
 
 #define LED_PIN 13
 
@@ -33,12 +27,22 @@ void error_loop(){
 
 void subscription_callback(const void * msgin)
 {  
-  const drive_controller_msgs__msg__Tank * msg1 = (const drive_controller_msgs__msg__Tank *)msgin;
-  Serial1.printf("%s: (%f,%f)\n", msg1->header.frame_id.data, msg1->left, msg1->right);
+//   const drive_controller_msgs__msg__Tank * msg1 = (const drive_controller_msgs__msg__Tank *)msgin;
+	const std_msgs__msg__Header * msg1 = (const std_msgs__msg__Header *)msgin;
+//   Serial1.printf("%s: (%f,%f)\n", msg1->header.frame_id.data, msg1->left, msg1->right);
+	Serial1.printf("%s: (%f,%f)\n", msg1->frame_id.data, msg1->stamp.sec, msg1->stamp.nanosec);
+	Serial1.printf("%s: (%f,%f)\n", msg.frame_id.data, msg.stamp.sec, msg.stamp.nanosec);
   digitalWrite(LED_PIN, !digitalRead(LED_PIN));  
 }
 
 void setup() {
+	rcl_subscription_t subscriber;
+	// drive_controller_msgs__msg__Tank msg;
+	
+	rclc_executor_t executor;
+	rclc_support_t support;
+	
+	rcl_node_t node;
 	Serial1.begin(9600);
   set_microros_transports();
   
@@ -47,7 +51,7 @@ void setup() {
   
   delay(2000);
 
-  allocator = rcl_get_default_allocator();
+  rcl_allocator_t allocator = rcl_get_default_allocator();
 
   //create init_options
   RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
@@ -59,15 +63,29 @@ void setup() {
   RCCHECK(rclc_subscription_init_default(
     &subscriber,
     &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(drive_controller_msgs, msg, Tank),
-    "micro_ros_arduino_subscriber"));
+    // ROSIDL_GET_MSG_TYPE_SUPPORT(drive_controller_msgs, msg, Tank),
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header),
+	"micro_ros_arduino_subscriber"));
 
   // create executor
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
   RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA));
+
+	// Assigning dynamic memory to the frame_id char sequence
+	msg.frame_id.capacity = 100;
+	msg.frame_id.data = (char*) malloc(msg.frame_id.capacity * sizeof(char));
+	msg.frame_id.size = 0;
+
+	// Assigning value to the frame_id char sequence
+	strcpy(msg.frame_id.data, "Hello World");
+	msg.frame_id.size = strlen(msg.frame_id.data);
+
+	while (1) {
+		delay(100);
+  		RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+	}
 }
 
 void loop() {
-  delay(100);
-  RCCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  
 }
